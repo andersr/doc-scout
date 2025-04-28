1ac143f26c4cd67d322b92a2eb44211a87cf9d66
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-04-28 15:55:01 -0400

    API test

--- app/routes/_main._index.tsx
+++ app/routes/_main._index.tsx
@@ -1,4 +1,4 @@
-import { prisma } from "~/.server/db";
+import { ENV } from "~/.server/ENV";
 import type { Route } from "./+types/_main._index";
 
 export function meta({}: Route.MetaArgs) {
@@ -10,8 +10,13 @@ export function meta({}: Route.MetaArgs) {
 
 export async function loader({ request }: Route.LoaderArgs) {
   try {
-    const users = await prisma.user.findMany({});
-    console.log(users);
+    // const users = await prisma.user.findMany({});
+    // console.log(users);
+    const res = await fetch(ENV.AI_API_URL);
+
+    const data = await res.json();
+    console.log("data: ", data);
+
     return { data: null };
   } catch (error) {
     console.error("error: ", error);
