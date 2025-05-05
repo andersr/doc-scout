30c73f4fbf664130b2f9331d40f1c5b78b310fac
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-05 05:26:09 -0400

    comment out AI api url for now

--- app/.server/ENV.ts
+++ app/.server/ENV.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 const envSchema = z.object({
   AUTH_SESSION_SECRET: z.string().min(3),
-  AI_API_URL: z.string().min(3),
+  // AI_API_URL: z.string().min(3),
 });
 
 export const ENV = envSchema.parse(process.env);
