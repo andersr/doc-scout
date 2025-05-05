9728f650e9f27c671b2c1402263f4c8c52bb88d1
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-05 13:44:46 -0400

    use getVectorStore

--- app/.server/langchain/generateGraph.ts
+++ app/.server/langchain/generateGraph.ts
@@ -1,52 +1,17 @@
 import type { Document } from "@langchain/core/documents";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { Annotation, StateGraph } from "@langchain/langgraph";
-import { PineconeStore } from "@langchain/pinecone";
-import { ENV } from "../ENV";
-import { pcClient } from "../pinecone/client";
-import { oaiEmbeddings } from "./embeddings";
+import { RAG_TEMPLATE } from "~/config/prompts";
 import { llm } from "./llm";
-// import { vectorStore } from "./vectorStore";
-
-interface ChatInput {
-  prompt: string;
-  context: string;
-}
-
-// function setPrompt({ prompt, context }: ChatInput) {
-//   return `
-//   Question: ${prompt}
-
-//   <CONTEXT>
-//   ${context}
-//   </CONTEXT>
-
-//   Use only the information in documents within the <CONTEXT> block to answer the question. List the corresponding documents used to provide an answer in the order of relevance.  If you are unable to provide an answer based on this information, say you don't know.
-
-//   Answer:  `;
-// }
-
-const RAG_TEMPLATE = `
-  Question: {question}
-
-  <CONTEXT>
-  {context}
-  </CONTEXT>
-
-  Use only the information within the <CONTEXT> block to answer the question.  If you are unable to provide an answer based on this information, say you don't know.
-    
-  Answer:  `;
+import { getVectorStore } from "./vectorStore";
 
 export async function generateGraph({
   collectionName,
 }: {
   collectionName: string;
 }) {
-  // Define prompt for question-answering
-  // const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
   const promptTemplate = PromptTemplate.fromTemplate(RAG_TEMPLATE);
 
-  // Define state for application
   const InputStateAnnotation = Annotation.Root({
     question: Annotation<string>,
   });
@@ -57,13 +22,14 @@ export async function generateGraph({
     answer: Annotation<string>,
   });
 
-  const pineconeIndex = pcClient.Index(collectionName, ENV.PINECONE_HOST);
+  // const pineconeIndex = pcClient.Index(collectionName, ENV.PINECONE_HOST);
 
-  const vectorStore = await PineconeStore.fromExistingIndex(oaiEmbeddings, {
-    pineconeIndex,
-    maxConcurrency: 5,
-    namespace: collectionName,
-  });
+  // const vectorStore = await PineconeStore.fromExistingIndex(oaiEmbeddings, {
+  //   pineconeIndex,
+  //   maxConcurrency: 5,
+  //   namespace: collectionName,
+  // });
+  const vectorStore = await getVectorStore(collectionName);
 
   // Define application steps
   const retrieve = async (state: typeof InputStateAnnotation.State) => {
@@ -80,9 +46,6 @@ export async function generateGraph({
     const response = await llm.invoke(messages);
     return { answer: response.content };
   };
-  // console.log("generate: ", generate);
-
-  // return generate;
 
   const graph = new StateGraph(StateAnnotation)
     .addNode("retrieve", retrieve)
