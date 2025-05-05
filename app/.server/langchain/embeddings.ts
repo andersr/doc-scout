06f0643858d9e014334d708b87121df91491fd71
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-05 13:44:58 -0400

    cleanup

--- app/.server/langchain/embeddings.ts
+++ app/.server/langchain/embeddings.ts
@@ -1,5 +1,5 @@
 import { OpenAIEmbeddings } from "@langchain/openai";
-import { EMBEDDING_MODEL } from "~/shared/embeddings";
+import { EMBEDDING_MODEL } from "~/config/embeddings";
 import { ENV } from "../ENV";
 
 export const oaiEmbeddings = new OpenAIEmbeddings({
--- app/.server/langchain/promptTemplate.ts
+++
@@ -1,8 +0,0 @@
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { pull } from "langchain/hub";
-
-export async function getPrompt() {
-  const promptTemplate = await pull<ChatPromptTemplate>("rlm/rag-prompt");
-  // console.log("promptTemplate: ", promptTemplate);
-  return promptTemplate;
-}
