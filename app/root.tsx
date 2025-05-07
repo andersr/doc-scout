a5bb19b7629768a49f95a9971183cfa75d562e64
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-07 06:53:45 -0400

    Replace react icons w material symbols

--- app/root.tsx
+++ app/root.tsx
@@ -7,6 +7,7 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import "material-symbols/outlined.css";
 import type { Route } from "./+types/root";
 import "./app.css";
 import { PageTitle } from "./components/PageTitle";
