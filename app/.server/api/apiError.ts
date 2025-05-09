e9589057078694312ced8f12307d4724d114e463
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-09 11:23:45 -0400

    handle ZodError in apiError

--- app/.server/api/apiError.ts
+++ app/.server/api/apiError.ts
@@ -1,19 +1,60 @@
 import { ReasonPhrases, StatusCodes } from "http-status-codes";
-import { data } from "react-router";
+import { ZodError } from "zod";
 import { APIError, type ApiResponse } from "~/types/api";
 
+const errorData: ApiResponse = {
+  errors: null,
+  ok: false,
+  data: null,
+};
+
 export function apiError(error: unknown) {
-  return data<ApiResponse>(
-    {
-      errorMessage:
+  if (error instanceof ZodError) {
+    // TODO: return actual Zod error
+    return new Response(JSON.stringify(errorData), {
+      status: 400,
+      statusText: "zod error",
+    });
+  }
+
+  if (error instanceof APIError) {
+    return new Response(JSON.stringify(errorData), {
+      status:
+        error instanceof APIError
+          ? error.statusCode
+          : StatusCodes.INTERNAL_SERVER_ERROR,
+      statusText:
         error instanceof APIError
           ? error.message
           : ReasonPhrases.INTERNAL_SERVER_ERROR,
-      ok: false,
-      data: null,
-    },
-    error instanceof APIError
-      ? error.statusCode
-      : StatusCodes.INTERNAL_SERVER_ERROR,
-  );
+    });
+  }
+
+  console.error("unknown error: ", error);
+  return new Response(JSON.stringify(errorData), {
+    status: StatusCodes.INTERNAL_SERVER_ERROR,
+    statusText: ReasonPhrases.INTERNAL_SERVER_ERROR,
+  });
 }
+
+// zod error sample
+//   issues: [
+//     {
+//       code: 'invalid_type',
+//       expected: 'string',
+//       received: 'undefined',
+//       path: [Array],
+//       message: 'Required'
+//     }
+//   ],
+//   addIssue: [Function (anonymous)],
+//   addIssues: [Function (anonymous)],
+//   errors: [
+//     {
+//       code: 'invalid_type',
+//       expected: 'string',
+//       received: 'undefined',
+//       path: [Array],
+//       message: 'Required'
+//     }
+//   ]
