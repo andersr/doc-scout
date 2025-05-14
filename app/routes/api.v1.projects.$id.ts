00ed1fae3c8d742af2ce5e852a805ed1a061f5f9
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-14 11:16:27 -0400

    cleanup

--- app/routes/api.v1.projects.$id.ts
+++ app/routes/api.v1.projects.$id.ts
@@ -2,20 +2,11 @@ import { data } from "react-router";
 import { apiError } from "~/.server/api/apiError";
 import { requireProjectViaKey } from "~/.server/projects/requireProjectViaKey";
 import { type ApiResponse } from "~/types/api";
-// import type { ProjectClient } from "~/types/project";
 import type { Route } from "../+types/root";
 
 export async function loader(args: Route.LoaderArgs) {
   try {
     const projectClient = await requireProjectViaKey(args);
-
-    // const consumerData: ProjectClient = {
-    //   projectName: project.name,
-    //   collectionName: project.collectionName ?? "", // for now
-    //   createdAt: project.createdAt ?? new Date(), // for now
-    //   publicId: project.publicId,
-    //   sources: project.sources.map((s) => ({ name: s.name, id: s.publicId })),
-    // };
 
     return data<ApiResponse>({
       errorMessage: "",
