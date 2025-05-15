6e7f289925edef6e905fe3714b479a9e67fdaa03
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-15 13:28:20 -0400

    DEV_HOST FIX

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), safeRoutes()],
   server: {
     port: 3000,
-    allowedHosts: process.env.DEV_HOST ? [process.env.DEV_HOST] : undefined,
+    ...(process.env.DEV_HOST && { allowedHosts: [process.env.DEV_HOST] }),
   },
 });
