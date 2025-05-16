67aacbd4e4654b0e4924bd5aaad0562eabc9fe23
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-16 10:32:38 -0400

    add public id to collection namespace

--- app/routes/_auth.collections.new.tsx
+++ app/routes/_auth.collections.new.tsx
@@ -165,7 +165,7 @@ export async function action(args: LoaderFunctionArgs) {
 
     await addDocsToVectorStore({
       docs,
-      collectionName: slugify(collection.name),
+      collectionName: `${collection.publicId}-${slugify(collection.name)}`,
     });
 
     return redirect(appRoutes("/collections/:id", { id: collection.publicId }));
