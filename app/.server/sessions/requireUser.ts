23bff166eb6962da365d404f79fad5197f2dea35
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-21 12:25:39 -0400

    sign out user for all requireUser errors

--- app/.server/sessions/requireUser.ts
+++ app/.server/sessions/requireUser.ts
@@ -45,7 +45,9 @@ export async function requireUser({
 
     if (!user) {
       console.error("No user in db");
-      throw redirect(appRoutes("/login"));
+      throw await logout({
+        request,
+      });
     }
 
     const email =
@@ -53,7 +55,9 @@ export async function requireUser({
 
     if (!email) {
       console.error("No user email");
-      throw redirect(appRoutes("/login"));
+      throw await logout({
+        request,
+      });
     }
 
     return {
