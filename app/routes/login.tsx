0c1637259a6d78fff3a8ad03bf6ae159ee1051c8
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-21 10:53:38 -0400

    add explicit redirect urls

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -7,6 +7,7 @@ import {
 } from "react-router";
 import { stytchClient } from "~/.server/stytch/client";
 import { upsertUser } from "~/.server/users/upsertUser";
+import { getDomainHost } from "~/.server/utils/getDomainHost";
 import { Button } from "~/components/ui/button";
 import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
@@ -76,9 +77,12 @@ export async function action(args: ActionFunctionArgs) {
         ok: false,
       };
     }
+    const redirectUrl = `${getDomainHost({ request, withProtocol: true })}/authenticate`;
 
     const res = await stytchClient.magicLinks.email.loginOrCreate({
       email,
+      login_magic_link_url: redirectUrl,
+      signup_magic_link_url: redirectUrl,
     });
 
     if (!res || !res.user_id) {
