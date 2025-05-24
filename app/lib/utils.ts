a61e26692e9b68d67b80b40914544fb4039ebdbc
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-24 08:59:12 -0400

    add vitest with sample test

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+// TODO: move, better name
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { safeRoutes } from "safe-routes/vite";
@@ -9,5 +10,8 @@ export default defineConfig({
   server: {
     port: 3000,
     ...(process.env.DEV_HOST && { allowedHosts: [process.env.DEV_HOST] }),
+  },
+  test: {
+    exclude: ["node_modules", "e2e", "tmp"],
   },
 });
