e39a18d411cf0f5798b023c550a4945ed81beee6
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-05-27 12:48:40 -0400

    refactor: :recycle: Rename NAMESPACE_TYPES to NAMESPACES

--- app/config/namespaces.ts
+++ app/config/namespaces.ts
@@ -1,6 +1,6 @@
-export const NAMESPACE_TYPES = {
-  USER: "user",
+export const NAMESPACES = {
+  user: "user",
 } as const;
 
-export const getNameSpace = (type: keyof typeof NAMESPACE_TYPES, id: string) =>
-  `${NAMESPACE_TYPES[type]}_${id}`;
+export const getNameSpace = (type: keyof typeof NAMESPACES, id: string) =>
+  `${NAMESPACES[type]}_${id}`;
