de00d598a1cd0dcd977a0119a699a1e168963b2f
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-03 06:36:43 -0400

    fix: 🛠️ remove email handling from getInitialsFromString

--- app/utils/getInitialsFromString.ts
+++ app/utils/getInitialsFromString.ts
@@ -1,14 +1,7 @@
 // TODO: better handling of getting initials from email - current approach is a hack
 
 export function getInitialsFromString(str: string) {
-  const isEmail = str.includes("@"); // TODO: use actual email regex
-  let parts = str.split(" ");
-
-  if (isEmail) {
-    const emailHandle = str.split("@")[0];
-
-    parts = emailHandle.split("");
-  }
+  const parts = str.split(" ");
   return parts.length > 1
     ? `${parts[0].replace(/\W/g, "").charAt(0)}${parts[parts.length - 1]
         .replace(/\W/g, "")
