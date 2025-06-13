f9b3c2f6e870acd153a042f7addb2eaddc5d33d8
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-13 08:36:37 -0400

    feat: :sparkles: add unit tests for getSession

--- app/.server/sessions/getSession/getSession.ts
+++ app/.server/sessions/getSession/getSession.ts
@@ -1,4 +1,4 @@
-import { authSessionStore } from "./authSessionStore";
+import { authSessionStore } from "../authSessionStore";
 
 export async function getSession({ request }: { request: Request }) {
   const cookie = request.headers.get("Cookie");
