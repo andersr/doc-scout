72246d25e67a05b3f90be1e761fd2e58df0aa253
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-24 13:28:23 -0400

    fix: :bug: actually add isAllowedUser to login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -3,6 +3,7 @@ import {
   type ActionFunctionArgs,
   Form,
   type LoaderFunctionArgs,
+  redirect,
   useActionData,
   useLoaderData,
   useNavigation,
@@ -11,12 +12,14 @@ import { upsertUser } from "~/.server/models/users/upsertUser";
 import { requireAnon } from "~/.server/services/sessions/requireAnon";
 
 import { getDomainHost } from "~/.server/utils/getDomainHost";
+import { isAllowedUser } from "~/.server/utils/isAllowedUser";
 import { stytchClient } from "~/.server/vendors/stytch/client";
 import { AppContainer } from "~/components/AppContainer";
 import AppHeader from "~/components/AppHeader";
 import { PageTitle } from "~/components/PageTitle";
 import { ActionButton } from "~/components/ui/ActionButton";
 import { Label } from "~/components/ui/label";
+import { appRoutes } from "~/shared/appRoutes";
 import { KEYS } from "~/shared/keys";
 import { INTENTIONALLY_GENERIC_ERROR_MESSAGE } from "~/shared/messages";
 
@@ -101,21 +104,18 @@ export async function action({ request }: ActionFunctionArgs) {
 
     const normalizedEmail = email.toLowerCase();
 
-    // const isAllowedUser=
+    const isAllowed = isAllowedUser(normalizedEmail);
 
-    // if (ENV.ALLOWED_USERS) {
-    //   const allowedUsers = ENV.ALLOWED_USERS.split(",");
-    //   if (!allowedUsers.includes(normalizedEmail)) {
-    //     return redirect(
-    //       `${getDomainHost({ request, withProtocol: true })}${appRoutes(
-    //         "/request-access",
-    //         {
-    //           email: normalizedEmail,
-    //         },
-    //       )}`,
-    //     );
-    //   }
-    // }
+    if (!isAllowed) {
+      return redirect(
+        `${getDomainHost({ request, withProtocol: true })}${appRoutes(
+          "/request-access",
+          {
+            email: normalizedEmail,
+          },
+        )}`,
+      );
+    }
 
     const isPreviewEnv = process.env.VERCEL_ENV === "preview";
     const redirectUrl = isPreviewEnv
