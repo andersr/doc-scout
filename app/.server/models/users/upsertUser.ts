268f720bb7228b42e8ef845a88cb483b91141f88
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-24 13:29:15 -0400

    fix: :lock: check for email OR stytch id on login

--- app/.server/models/users/upsertUser.ts
+++ app/.server/models/users/upsertUser.ts
@@ -1,8 +1,6 @@
 import type { User } from "@prisma/client";
-import { ReasonPhrases, StatusCodes } from "http-status-codes";
 import { generateId } from "~/.server/utils/generateId";
 import { prisma } from "~/lib/prisma";
-import { ServerError } from "~/types/server";
 
 export async function upsertUser({
   email,
@@ -10,19 +8,26 @@ export async function upsertUser({
 }: Pick<User, "stytchId"> & { email: string }) {
   const existingUser = await prisma.user.findFirst({
     where: {
-      email: {
-        equals: email,
-        mode: "insensitive",
-      },
+      OR: [
+        {
+          email: {
+            equals: email,
+            mode: "insensitive",
+          },
+        },
+        {
+          stytchId,
+        },
+      ],
     },
   });
 
-  if (existingUser && existingUser?.stytchId !== stytchId) {
-    console.warn(
-      `Existing Stytch id for user ${email} does not match incoming stytch id.`,
-    );
-    throw new ServerError(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST);
-  }
+  // if (existingUser && existingUser?.stytchId !== stytchId) {
+  //   console.warn(
+  //     `Existing Stytch id for user ${email} does not match incoming stytch id.`,
+  //   );
+  //   throw new ServerError(ReasonPhrases.BAD_REQUEST, StatusCodes.BAD_REQUEST);
+  // }
 
   if (!existingUser) {
     await prisma.user.create({
