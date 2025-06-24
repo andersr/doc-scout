0b192834c15b60eba23794285d8574381b1f5bfa
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-24 15:18:37 -0400

    refactor: :recycle: simplify isAllowedUser function logic

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -10,8 +10,8 @@ import {
 } from "react-router";
 import { upsertUser } from "~/.server/models/users/upsertUser";
 import { requireAnon } from "~/.server/services/sessions/requireAnon";
-
 import { getDomainHost } from "~/.server/utils/getDomainHost";
+
 import { isAllowedUser } from "~/.server/utils/isAllowedUser";
 import { stytchClient } from "~/.server/vendors/stytch/client";
 import { AppContainer } from "~/components/AppContainer";
