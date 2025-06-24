a9ec780622f0a6a345358cf20ffabeaf24a760a1
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-24 16:23:39 -0400

    fix: prepend 'anders+' to generated test email addresses

--- e2e/utils/getTestEmail.ts
+++ e2e/utils/getTestEmail.ts
@@ -1,4 +1,4 @@
 const TEST_DOMAIN = "anders.co";
 
 export const getTestEmail = (user: string) =>
-  `${user}${process.env.CI ? "+ci" : ""}@${TEST_DOMAIN}`;
+  `anders+${user}${process.env.CI ? ".ci" : ""}@${TEST_DOMAIN}`;
