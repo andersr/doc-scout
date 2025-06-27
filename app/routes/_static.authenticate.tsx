0c011ff695c368c55139a8d91ea91953ca504613
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-27 07:16:10 -0400

    fix: :wrench: add error boundary to static routes

--- app/routes/_static.authenticate.tsx
+++ app/routes/_static.authenticate.tsx
@@ -1,4 +1,4 @@
-import { Link, type LoaderFunctionArgs, useLoaderData } from "react-router";
+import { Link, type LoaderFunctionArgs } from "react-router";
 import { createSession } from "~/.server/services/sessions/createSession";
 import { stytchClient } from "~/.server/vendors/stytch/client";
 import { PageTitle } from "~/components/PageTitle";
@@ -7,64 +7,46 @@ import {
   STYTCH_SESSION_TOKEN,
 } from "~/config/auth";
 import { appRoutes } from "~/shared/appRoutes";
-import { INTENTIONALLY_GENERIC_ERROR_MESSAGE } from "~/shared/messages";
 import { LINK_STYLES } from "~/styles/links";
 
 export async function loader(args: LoaderFunctionArgs) {
   const { request } = args;
-  try {
-    const searchParams = new URL(request.url).searchParams;
-    const token = searchParams.get("token");
-    const tokenType = searchParams.get("stytch_token_type");
+  const searchParams = new URL(request.url).searchParams;
+  const token = searchParams.get("token");
+  const tokenType = searchParams.get("stytch_token_type");
 
-    if (!token) {
-      console.error(`No token`);
-      return new Response("Bad request", {
-        status: 400,
-      });
-    }
-    if (tokenType !== "magic_links") {
-      console.error(`Unsupported token type: '${tokenType}'`);
-      return new Response("Bad request", {
-        status: 400,
-      });
-    }
-
-    const res = await stytchClient.magicLinks.authenticate({
-      session_duration_minutes: STYTCH_SESSION_DURATION_MINUTES,
-      token,
+  if (!token) {
+    console.error(`No token`);
+    return new Response("Bad request", {
+      status: 400,
     });
-
-    return createSession({
-      key: STYTCH_SESSION_TOKEN,
-      redirectTo: appRoutes("/"),
-      request,
-      token: res.session_token,
-    });
-  } catch (error) {
-    console.error("Authenticate error: ", error);
-    return {
-      errorMessage:
-        error instanceof Error && error.message
-          ? error.message
-          : INTENTIONALLY_GENERIC_ERROR_MESSAGE,
-      ok: false,
-    };
   }
+  if (tokenType !== "magic_links") {
+    console.error(`Unsupported token type: '${tokenType}'`);
+    return new Response("Bad request", {
+      status: 400,
+    });
+  }
+
+  const res = await stytchClient.magicLinks.authenticate({
+    session_duration_minutes: STYTCH_SESSION_DURATION_MINUTES,
+    token,
+  });
+
+  return createSession({
+    key: STYTCH_SESSION_TOKEN,
+    redirectTo: appRoutes("/"),
+    request,
+    token: res.session_token,
+  });
 }
 
 export default function AuthenticateRoute() {
-  const { errorMessage } = useLoaderData<typeof loader>();
   return (
     <div>
-      <div className="text-center">
+      <div>
         <PageTitle title={"Auth Error"} />
       </div>
-      {errorMessage && (
-        <div className="mt-4 text-center font-semibold text-red-400">
-          {errorMessage}
-        </div>
-      )}
       <div>
         <Link className={LINK_STYLES} to={appRoutes("/login")}>
           Back to Login
