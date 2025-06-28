ecc00544abeb793f5e5d8f5542ff45d86632c84a
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-06-28 12:13:44 -0400

    test: 🧪 add logout e2e test

--- e2e/tests/userMenu.spec.ts
+++ e2e/tests/userMenu.spec.ts
@@ -1,14 +1,23 @@
-import { test } from "@playwright/test";
+import { expect, test } from "@playwright/test";
+import { setAuthStoragePath } from "e2e/utils/setAuthStoragePath";
+import { appRoutes } from "~/shared/appRoutes";
+import { TEST_USERS } from "~/types/testUsers";
 
 test.describe("User Menu", () => {
+  test.use({ storageState: setAuthStoragePath(TEST_USERS.no_docs) });
+
   test.skip(`displays current user's email address`, async ({ page }) => {
     // click on user menu
     // expect to see email address
   });
 
-  test.skip(`signs out a user if clicking sign out`, async ({ page }) => {
-    // click on user menu
-    // click sign out
-    // expect to see login page
+  test(`signs out a user if clicking sign out`, async ({ page }) => {
+    // act
+    await page.goto(appRoutes("/"));
+    await page.getByRole("button", { name: "no_docs@test.com" }).click();
+    await page.getByRole("button", { name: "Sign Out" }).click();
+
+    // assert
+    await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
   });
 });
