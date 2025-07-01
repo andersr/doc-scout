520949b5827cf785f1436556ee9985c30cae01ff
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-01 14:59:00 -0400

    fix: :eyes: disable login submit if input is empty

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -83,7 +83,7 @@ export default function LoginRoute() {
               required
             />
           </div>
-          <ActionButton type="submit">
+          <ActionButton type="submit" disabled={nameValue.trim() === ""}>
             {navigation.state === "submitting" ? "Loading..." : "Continue"}
           </ActionButton>
         </Form>
