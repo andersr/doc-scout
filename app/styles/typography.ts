fd6d3ec3dc02fddb5469beee1e71aa9266f8878d
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-07 09:28:30 -0400

    fix: :wrench: email typo, simplify prose styles

--- app/styles/typography.ts
+++ app/styles/typography.ts
@@ -1,2 +1,1 @@
-export const PROSE_STYLES =
-  "prose prose-xl prose-h1:mt-4 md:prose-h1:mt-0 prose-h1:mb-2 prose-h2:mb-4 prose-h3:mb-1 prose-h3:font-semibold prose-h3:text-2xl prose-h3:mt-4 prose-h2:mt-10 prose-p:leading-normal prose-li:mt-0 prose-img:mt-0";
+export const PROSE_STYLES = "prose";
