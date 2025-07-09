ceff2d14af57c3c77bf8cd775be2adf1e8641c4c
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-09 13:46:07 -0400

    feat: :wrench: add google drive env vars - also DRY up reused keySchema

--- app/.server/ENV.ts
+++ app/.server/ENV.ts
@@ -1,25 +1,30 @@
 import { z } from "zod";
 
+const keySchema = z.string().min(3);
+
 const envSchema = z.object({
-  ADOBE_PDF_SERVICES_CLIENT_ID: z.string().min(3),
-  ADOBE_PDF_SERVICES_CLIENT_SECRET: z.string().min(3),
-  ALLOWED_USERS: z.string().min(3),
-  AUTH_SESSION_SECRET: z.string().min(3),
-  AWS_ACCESS_KEY_ID: z.string().min(3),
+  ADOBE_PDF_SERVICES_CLIENT_ID: keySchema,
+  ADOBE_PDF_SERVICES_CLIENT_SECRET: keySchema,
+  ALLOWED_USERS: keySchema,
+  AUTH_SESSION_SECRET: keySchema,
+  AWS_ACCESS_KEY_ID: keySchema,
   AWS_CDN_HOST: z.string().url(),
-  AWS_CDN_PRIVATE_KEY_NAME: z.string().min(3),
-  AWS_CDN_PUBLIC_KEY_ID: z.string().min(3),
-  AWS_REGION: z.string().min(3),
-  AWS_S3_BUCKET_NAME: z.string().min(3),
-  AWS_SECRET_ACCESS_KEY: z.string().min(3),
-  FIRECRAWL_API_KEY: z.string().min(3),
-  OPENAI_API_KEY: z.string().min(3),
-  PINECONE_API_KEY: z.string().min(3),
-  PINECONE_HOST: z.string().min(3),
-  PINECONE_INDEX_NAME: z.string().min(3),
-  STYTCH_PROJECT_ID: z.string().min(3),
-  STYTCH_PUBLIC_TOKEN: z.string().min(3),
-  STYTCH_SECRET: z.string().min(3),
+  AWS_CDN_PRIVATE_KEY_NAME: keySchema,
+  AWS_CDN_PUBLIC_KEY_ID: keySchema,
+  AWS_REGION: keySchema,
+  AWS_S3_BUCKET_NAME: keySchema,
+  AWS_SECRET_ACCESS_KEY: keySchema,
+  FIRECRAWL_API_KEY: keySchema,
+  GOOGLE_APP_ID: keySchema,
+  GOOGLE_CLIENT_ID: keySchema,
+  GOOGLE_DRIVE_API_KEY: keySchema,
+  OPENAI_API_KEY: keySchema,
+  PINECONE_API_KEY: keySchema,
+  PINECONE_HOST: keySchema,
+  PINECONE_INDEX_NAME: keySchema,
+  STYTCH_PROJECT_ID: keySchema,
+  STYTCH_PUBLIC_TOKEN: keySchema,
+  STYTCH_SECRET: keySchema,
 });
 
 export const ENV = envSchema.parse(process.env);
