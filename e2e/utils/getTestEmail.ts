d5c54bcc6be9a038126db7f1241d9c2e6498d11f
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-12 14:27:17 -0400

    test: 🧪 rm unneeded domain name const

--- e2e/utils/getTestEmail.ts
+++ e2e/utils/getTestEmail.ts
@@ -1,8 +1,5 @@
 import type { TestUserNames } from "@e2e/types/testUsers";
 
-// TODO: move to common config
-const TEST_DOMAIN = "test.com";
-
 export const getTestEmail = (username: TestUserNames) => {
-  return `${username}@${TEST_DOMAIN}`;
+  return `${username}@test.com`;
 };
