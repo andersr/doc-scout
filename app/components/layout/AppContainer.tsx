f96425cf61dc641682fadbb4b64e1729142fed96
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-12 05:37:31 -0400

    style: :lipstick: footer padding bottom fix

--- app/components/layout/AppContainer.tsx
+++ app/components/layout/AppContainer.tsx
@@ -12,7 +12,7 @@ export function AppContainer({ children, className }: Props) {
   return (
     <div className={twMerge("flex h-full flex-col p-4", className)}>
       {children}
-      <footer className="flex items-center justify-center gap-4">
+      <footer className="flex items-center justify-center gap-4 pb-2 md:pb-4">
         <Link className={MUTED_LINK_STYLES} to={appRoutes("/privacy")}>
           Privacy
         </Link>
