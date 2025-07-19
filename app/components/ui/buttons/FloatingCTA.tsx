0258bd1ce689c9be63dc60761e6f38b117bac46f
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2025-07-19 12:38:51 -0400

    style: :lipstick: floating cta offset tweak

--- app/components/ui/buttons/FloatingCTA.tsx
+++ app/components/ui/buttons/FloatingCTA.tsx
@@ -11,7 +11,7 @@ export function FloatingCTA({
   to: string;
 }) {
   return (
-    <div className="fixed right-0 bottom-14 left-0 z-20 flex justify-center md:bottom-24">
+    <div className="fixed right-0 bottom-18 left-0 z-20 flex justify-center md:bottom-24">
       <Link
         className={twMerge(
           "bg-navy-blue flex items-center justify-center rounded-xl py-3 pr-6 pl-3 font-medium text-white drop-shadow-lg md:py-3 md:pr-8 md:pl-4 md:text-lg",
